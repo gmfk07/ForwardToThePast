69fb647c67085545e2dc6656335fc8ada005a4c8
Julian Hernandez <gmfk07@gmail.com>
2018-05-05 16:10:51 -0400

    Players, Walls, and Crates

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        Transform t = GetComponent<Transform>();
+        float vertInput = Input.GetAxis("Vertical");
+        float horInput = Input.GetAxis("Horizontal");
+
+        t.position += new Vector3(horInput * speed, vertInput * speed);
+    }
+}
