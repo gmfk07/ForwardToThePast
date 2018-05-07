8378b50d5687fc3cfa3b4dd664ae916f59dd0fb5
Julian Hernandez <gmfk07@gmail.com>
2018-05-06 22:11:18 -0400

    Camera Moving

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform player;
+    public float width;
+    public float height;
+	
+	// Update is called once per frame
+	void Update () {
+        var playerPos = player.transform.position;
+        var camPos = gameObject.transform.position;
+        if (playerPos.x > camPos.x + width/2)
+            MoveCam(width, 0);
+        if (playerPos.x < camPos.x - width/2)
+            MoveCam(-width, 0);
+        if (playerPos.y > camPos.y + height / 2)
+            MoveCam(0, height);
+        if (playerPos.y < camPos.y - height / 2)
+            MoveCam(0, -height);
+	}
+
+    void MoveCam(float xmove, float ymove) {
+        var camPos = transform;
+        camPos.Translate(Vector3.right * xmove);
+        camPos.Translate(Vector3.up * ymove);
+    }
+}
