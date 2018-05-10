618fe5bf0f8be5d90598b0404b7c8aec4f71f3b1
Julian Hernandez <gmfk07@gmail.com>
2018-05-10 17:31:08 -0400

    Sorry, Attacking BROKE

---
+++ Assets/Scripts/EnemyBasic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBasic : MonoBehaviour {
+
+    public int health;
+    private PlayerStats playerstats;
+
+    public void Damage()
+    {
+        health--;
+    }
+
+    void OnCollisionStay2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerstats = collider.gameObject.GetComponent<PlayerStats>();
+            if (!playerstats.invincible)
+            {
+                playerstats.health--;
+                playerstats.invincible = true;
+                playerstats.StartTimer();
+            }
+        }
+    }
+}
--- Assets/Scripts/SwordSlash.cs
+++ Assets/Scripts/SwordSlash.cs
@@ -3,7 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwordSlash : MonoBehaviour {
+    private List<Collider2D> hitList;
+
     void Start () {
         Destroy(gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Enemy" && !hitList.Contains(collider))
+        {
+            collider.gameObject.GetComponent<EnemyBasic>().Damage();
+            hitList.Add(collider);
+        }
     }
 }
