af3c001551e061b4967ee7d350dbc5bf83cb8d46
Andrew Hoyt <andrew@chillhammer.com>
2018-05-11 22:18:15 -0400

    Beginning of NPC
    Will work on Dialog. Can walk up to NPC and attempt talk.

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    public string[] texts;
+    public float interactRadius = 2f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    //Can be overriden for custom interaction
+    public virtual void Talk() {
+        Debug.Log("Talk, talk, talk.");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, interactRadius);
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    #region Singleton
+    public static PlayerManager instance;
+    void Awake()
+    {
+        instance = this;
+    }
+    #endregion Singleton
+
+    GameObject player;
+    public static GameObject Player { get { return instance.player; } }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+}
