590b086b31ba66f5f1942e31a97000a04d8cd913
Julian Hernandez <gmfk07@gmail.com>
2018-05-11 22:56:35 -0400

    Health pickups and projectiles

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+    public int money = 0;
+    public int health = 0;
+
+	void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            collider.gameObject.GetComponent<PlayerStats>().money += money;
+            collider.gameObject.GetComponent<PlayerStats>().IncreaseHealth(health);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    public float timer = 1.0f;
+
+    private void Start()
+    {
+        StartCoroutine(DeathTimer());
+    }
+
+    IEnumerator DeathTimer()
+    {
+        yield return new WaitForSeconds(timer);
+        Destroy(gameObject);
+    }
+
+    void OnCollisionStay2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Player" || collider.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
