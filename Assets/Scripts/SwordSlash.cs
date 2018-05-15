bed13cd22fc36504adf71d549b9b03c9eadb7797
ahmed176 <ahmed176@mit.edu>
2018-05-15 18:41:24 -0400

    more map and other changes
    added breakable wall layer
    bullets now stop on breakable wall
    sword slashes destroy projectiles
    expanded map

--- Assets/Scripts/SwordSlash.cs
+++ Assets/Scripts/SwordSlash.cs
@@ -17,7 +17,11 @@ public class SwordSlash : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D collider)
     {
         int colliderId = collider.GetInstanceID();
-        if (collider.gameObject.layer == 8 && !hitList.Contains(colliderId))
+        if(collider.gameObject.layer == 9)
+        {
+            Destroy(collider.gameObject);
+        }
+        if ((collider.gameObject.layer == 8 || collider.gameObject.layer == 10) && !hitList.Contains(colliderId))
         {
             collider.gameObject.GetComponent<EnemyBasic>().TakeDamage();
             hitList.Add(colliderId);
